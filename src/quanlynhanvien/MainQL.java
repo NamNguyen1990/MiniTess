68ed001fe3dd612307655d9e103d3ca746c84e12
NamNguyen1990 <namxuan.nguyen1990@gmail.com>
2022-05-01 17:33:12 +0700

    Sualan2

--- src/quanlynhanvien/MainQL.java
+++ src/quanlynhanvien/MainQL.java
@@ -11,6 +11,7 @@ public class MainQL {
         Scanner sc = new Scanner(System.in);
         Scanner nhapChu = new Scanner(System.in);
         Scanner nhapChu1 = new Scanner(System.in);
+        Scanner nhapChu2 = new Scanner(System.in);
 
         int luaChon = -1;
 
@@ -19,27 +20,31 @@ public class MainQL {
         System.out.println("0 - Hiển thị tất cả Nhân viên");
         System.out.println("1 - Thêm 1 Nhân viên mới");
         System.out.println("2 - Tìm kiếm thông tin nhân viên theo số CMND");
-        System.out.println("3 - Tìm kiếm thông tin nhân viên theo tên");
-        System.out.println("4 - In ra danh sách nhân viên Fulltime");
-        System.out.println("5 - In ra danh sách nhân viên Parttime");
-        System.out.println("6 - Sửa thông tin nhân viên theo Tên nhập vào");
+        System.out.println("3 - Tìm kiếm thông tin nhân viên theo tên");  // Tìm kiếm và hiển thị tất cả nhân viên có tên giống nhau!
+        System.out.println("4 - In ra danh sách nhân viên Fulltime/Parttime");
+//        System.out.println("5 - In ra danh sách nhân viên Parttime");
+        System.out.println("6 - Sửa thông tin nhân viên theo Tên nhập vào"); // Chỉ sửa được người đầu tiên tìm thấy!
         System.out.println("7 - Sửa thông tin nhân viên theo số CMND nhập vào");
-        System.out.println("8 - Thay đổi trạng thái nhân viên");
-        System.out.println("9 - Lương nhân viên");
+        System.out.println("8 - Thay đổi trạng thái nhân viên (Từ Đang làm --> Thôi việc, hoặc ngược lại)");
+        System.out.println("9 - Hiển thị lương nhân viên");
         System.out.println("10 - Hiển thị danh sách nhân viên có sẵn");
-        System.out.println("11 - Xóa vĩnh viễn 1 nhân viên trong danh sách");
+        System.out.println("11 - Xóa vĩnh viễn 1 nhân viên trong danh sách theo số CMND");
         System.out.println("12 - Thoát");
 
         do {
-            System.out.println("Mời bạn chọn chức năng");
-            try {
-                luaChon = sc.nextInt();
-                if (luaChon<0 || luaChon >12) throw new Exception();
-            } catch (InputMismatchException e) {
-                System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
-                sc.nextLine();
-            } catch (Exception e) {
-                System.out.println(ANSI_RED + "Chỉ được nhập số từ 0 --> 12" + ANSI_RESET);
+            boolean check2 = false;
+            while (!check2) {
+                System.out.println("Mời bạn chọn chức năng");
+                try {
+                    luaChon = sc.nextInt();
+                    if (luaChon<0 || luaChon >12) throw new Exception();
+                    check2 = true;
+                } catch (InputMismatchException e) {
+                    System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                    sc.nextLine();
+                } catch (Exception e) {
+                    System.out.println(ANSI_RED + "Chỉ được nhập số từ 0 --> 12" + ANSI_RESET);
+                }
             }
 
             if (luaChon == 0) {
@@ -81,10 +86,9 @@ public class MainQL {
 
                 NhanVien nhanVien = new NhanVien(soCMND,ten,loai,trangThai,luong);
 
-
-                System.out.println("Bạn có muốn thêm người này vào danh sách nhân viên không?" + ANSI_RED + "(Chọn: Y/N)" + ANSI_RESET);
+                System.out.println("Bạn có muốn thêm người này vào danh sách nhân viên không?" + ANSI_RED + "[Chọn: Y/E(Exit) hoặc nhập bất kỳ để không thêm]" + ANSI_RESET);
                 System.out.println(ANSI_RED + "NOTE: Nhập 10 lần đầu tiên để hiện thị nhân viên có sẵn trước khi thêm mới, nếu không sẽ mất toàn bộ dữ liệu nhân viên!" + ANSI_RESET);
-                char result, a = 'Y', b = 'N';
+                char result, a = 'Y', b = 'E';
                 result = nhapChu1.next().charAt(0);
                 if (result == a) {
                     quanLyNhanVien.them(nhanVien);
@@ -94,30 +98,150 @@ public class MainQL {
                     System.exit(0);
                 }
 
-
 //                quanLyNhanVien.them(nhanVien);
 //                GhiDocFile.writeToFile("KtraMD2.csv",quanLyNhanVien.nhanVienList);
-
             }
             else if (luaChon == 2) {
+                System.out.println("Nhập số CMND cần tìm");
+                int soCMND = sc.nextInt();
+                quanLyNhanVien.timKiemCMND(soCMND);
 
             }
             else if (luaChon == 3) {
-
+                System.out.println("Nhập tên nhân viên cần tìm");
+                String ten = nhapChu.nextLine();
+                quanLyNhanVien.timKiemTen(ten);
             }
             else if (luaChon == 4) {
+                System.out.println("Bạn muốn in nhân viên làm Fulltime hay Parttime?" + ANSI_RED + "[Chọn: F/P/E(Exit) hoặc nhập bất kỳ để thoát]" + ANSI_RESET);
+                char result, a = 'F', b = 'P', c = 'E';
+                result = nhapChu2.next().charAt(0);
 
+                if (result == a) {
+                    quanLyNhanVien.timKiemFullTime();
+                }
+                if (result == b) {
+                    quanLyNhanVien.timKiemPartTime();
+                }
+                if (result == c) {
+                    System.exit(0);
+                }
+//                quanLyNhanVien.timKiemFullTime();
             }
-            else if (luaChon == 5) {
-
-            }
+//            else if (luaChon == 5) {
+//                quanLyNhanVien.timKiemPartTime();
+//            }
             else if (luaChon == 6) {
+                System.out.println("Nhập Tên muốn sửa");
+                String tenS = nhapChu.nextLine();
+
+                int soCMNDM = -1;
+                boolean check3 = false;
+                while (!check3) {
+                    System.out.println("Nhập Số CMND mới");
+                    try {
+                        soCMNDM = sc.nextInt();
+                        check3 = true;
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                        sc.nextLine();
+                    }
+                }
+                System.out.println("Nhập Tên mới");
+                String tenM = nhapChu.nextLine();
+                System.out.println("Nhập Loại mới");
+                String loaiM = nhapChu.nextLine();
+                System.out.println("Nhập Trạng Thái mới");
+                String trangThaiM = nhapChu.nextLine();
+
+                int luongM = -1;
+                boolean check4 = false;
+                while (!check4) {
+                    System.out.println("Nhập Lương mới");
+                    try {
+                        luongM = sc.nextInt();
+                        check4 = true;
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                        sc.nextLine();
+                    }
+                }
+
+                NhanVien nhanVien1 = new NhanVien(soCMNDM,tenM,loaiM,trangThaiM,luongM);
+                quanLyNhanVien.suaTen(tenS,nhanVien1);
+                GhiDocFile.writeToFile("KtraMD2.csv",quanLyNhanVien.nhanVienList);
 
             }
             else if (luaChon == 7) {
+                int soCMNDS = -1;
+                boolean check5 = false;
+                while (!check5) {
+                    System.out.println("Nhập số CMND cần tìm sửa");
+                    try {
+                        soCMNDS = sc.nextInt();
+                        check5 = true;
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                        sc.nextLine();
+                    }
+                }
+
+                int soCMNDM = -1;
+                boolean check6 = false;
+                while (!check6) {
+                    System.out.println("Nhập vào số CMND mới");
+                    try {
+                        soCMNDM = sc.nextInt();
+                        check6 = true;
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                        sc.nextLine();
+                    }
+                }
+
+                System.out.println("Nhập vào tên mới");
+                String tenM = nhapChu.nextLine();
+                System.out.println("Nhập vào loại mới");
+                String loaiM = nhapChu.nextLine();
+                System.out.println("Nhập vào trạng thái mới");
+                String trangThaiM = nhapChu.nextLine();
+
+                int luongM = -1;
+                boolean check7 = false;
+                while (!check7) {
+                    System.out.println("Nhập vào lương mới");
+                    try {
+                        luongM = sc.nextInt();
+                        check7 = true;
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                        sc.nextLine();
+                    }
+                }
+
+                NhanVien nhanVien2 = new NhanVien(soCMNDM,tenM,loaiM,trangThaiM,luongM);
+                quanLyNhanVien.suasoCMND(soCMNDS,nhanVien2);
+                GhiDocFile.writeToFile("KtraMD2.csv",quanLyNhanVien.nhanVienList);
+                System.out.println("Bạn đã sửa thành công thông tin nhân viên trên");
 
             }
             else if (luaChon == 8) {
+
+                int soCMNDCN = -1;
+                boolean check8 = false;
+                while (!check8) {
+                    System.out.println("Nhập số CMND của nhân viên cần cập nhật trạng thái!");
+                    try {
+                        soCMNDCN = sc.nextInt();
+                        check8 = true;
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Chỉ được nhập số" + ANSI_RESET);
+                        sc.nextLine();
+                    }
+                }
+
+                    quanLyNhanVien.capNhatsoCMND(soCMNDCN);
+                    GhiDocFile.writeToFile("KtraMD2.csv",quanLyNhanVien.nhanVienList);
 
             }
             else if (luaChon == 9) {
--- src/quanlynhanvien/QuanLyNhanVien.java
+++ src/quanlynhanvien/QuanLyNhanVien.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class QuanLyNhanVien {
      List<NhanVien> nhanVienList = new ArrayList<>();
+    public static final String FULL = "Fulltime";
+    public static final String PART = "Parttime";
 
     public List<NhanVien> getNhanVienList() {
         return nhanVienList;
@@ -28,4 +30,121 @@ public class QuanLyNhanVien {
             System.out.println("Chưa có nhân viên nào được thêm mới!");
         }
     }
+
+    public void timKiemTen(String ten) {  // Cái này trả về các vị trí(hiển thị luôn thông tin) phục vụ cho việc tìm kiếm theo tên nhập vào
+        boolean check = false;
+        for (int i=0; i<nhanVienList.size(); i++) {
+            if (nhanVienList.get(i).getTen().contains(ten)) {
+                System.out.println(nhanVienList.get(i));
+                check = true;
+            }
+        }
+        if (check == false) {
+            System.out.println("Không có nhân viên này");
+        }
+    }
+
+    public void timKiemCMND(int soCMND) {   // Cái này trả về vị trí(hiển thị luôn thông tin) phục vụ cho tìm kiếm theo số CMND
+        boolean check = false;
+        for (int i=0; i<nhanVienList.size(); i++) {
+            if (nhanVienList.get(i).getSoCMND() == soCMND) {
+                System.out.println(nhanVienList.get(i));
+                check = true;
+            }
+        }
+        if (check == false) {
+            System.out.println("Không có nhân viên này");
+        }
+    }
+
+    public void timKiemFullTime() {   // Cái này trả về các vị trí(hiển thị luôn thông tin) phục vụ cho tìm kiếm theo Fulltime
+        boolean check = false;
+        for (int i=0; i<nhanVienList.size(); i++) {
+            if (nhanVienList.get(i).getLoai().equals(FULL)) {
+                System.out.println(nhanVienList.get(i));
+                check = true;
+            }
+        }
+        if (check == false) {
+            System.out.println("Không có nhân viên làm Fulltime");
+        }
+    }
+
+    public void timKiemPartTime() {  // Cái này trả về các vị trí(hiển thị luôn thông tin) phục vụ cho tìm kiếm theo Parttime
+        boolean check = false;
+        for (int i=0; i<nhanVienList.size(); i++) {
+            if (nhanVienList.get(i).getLoai().equals(PART)) {
+                System.out.println(nhanVienList.get(i));
+                check = true;
+            }
+        }
+        if (check == false) {
+            System.out.println("Không có nhân viên làm Parttime");
+        }
+    }
+
+    public int timKiemTen1(String ten) {  // Cái này trả về vị trí (chỉ vị trí ko hiển thị thông tin) phục vụ cho việc sửa theo tên nhập vào (chỉ có tác dụng khi tên là duy nhất)
+        for (int i = 0; i < nhanVienList.size(); i++) {
+            if (nhanVienList.get(i).getTen().equals(ten)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void suaTen(String ten, NhanVien nhanVien) { // Cái này sửa theo tên nhập vào (chỉ có tác dụng khi tên là duy nhất)
+        int indexOf = timKiemTen1(ten);
+        if (indexOf == -1) {
+            System.out.println("Không có Nhân viên này");
+        } else {
+            nhanVienList.set(indexOf, nhanVien);
+        }
+    }
+
+    public int timKiemsoCMND (int soCMND) {  // // Cái này trả về vị trí (chỉ vị trí ko hiển thị thông tin) phục vụ cho việc sửa theo số CMND nhập vào
+        for (int i = 0; i < nhanVienList.size(); i++) {
+            if (nhanVienList.get(i).getSoCMND() == soCMND) {
+               return i;
+            }
+        }
+        return -1;
+    }
+
+    public void suasoCMND (int soCMND, NhanVien nhanVien) { // Cái này sửa theo số CMND nhập vào
+        int indexOf = timKiemsoCMND(soCMND);
+        if (indexOf == -1) {
+            System.out.println("Không có Nhân viên nào có số CMND này");
+        } else {
+            nhanVienList.set(indexOf, nhanVien);
+        }
+    }
+
+    public void capNhatsoCMND (int soCMND) { // Cái này sửa theo số CMND nhập vào
+        int indexOf = timKiemsoCMND(soCMND);
+        if (indexOf == -1) {
+            System.out.println("Không có Nhân viên nào có số CMND này");
+        } else {
+            nhanVienList.get(indexOf).setTrangThai("Thôi việc");
+            nhanVienList.get(indexOf).setLuong(0);
+            nhanVienList.get(indexOf).setLoai("Thôi việc");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
